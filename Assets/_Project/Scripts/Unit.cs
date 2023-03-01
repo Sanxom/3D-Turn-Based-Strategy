69888f598c64727e4334bec320b8b7b43b0c8f98
Sanxom <masonbell1303@gmail.com>
2023-03-01 01:53:46 -0600

    First Commit w/ Imports and Unit Movement
    - First Commit
    - Added basic plane with a grid to help define "tiles" later
    - Added basic Unit click-to-move movement based on a Ray which gets the mouse position from the camera and takes into account a LayerMask for the Floor GameObject
    - Added a Rifle asset to the test Unit's Right Hand and positioned it accordingly
    - Added Rifle Run and Rifle Aiming Idle animations to the test Unit
    - Created IsWalking parameter in the test Unit's Animator to transition between Idle and Running

---
+++ Assets/_Project/Scripts/Unit.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    private float moveSpeed = 4f;
+
+    private void Update()
+    {
+        float stoppingDistance = 0.1f;
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            transform.position += moveSpeed * Time.deltaTime * moveDirection;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Move(MouseWorld.GetPosition());
+        }
+    }
+
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+}
